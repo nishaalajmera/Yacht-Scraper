1531130017e1e196613202c99bb7fc1c8b7f3de2
nishaalajmera <nishaal.ajmera@gmail.com>
2021-10-05 19:20:59 +0100

    Change dependencies

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages
 
 setup(
     name = 'yacht_scraper',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Package that allows scraping of yacht data from boat24.com',
     url = 'https://github.com/nishaalajmera/Yacht_scraper.git',
     author = 'Nishaal Ajmera',
     licence = 'MIT',
     packages = find_packages(),
-    install_requires = ['selenium','pandas','tqdm','os']
+    install_requires = ['selenium','pandas','tqdm']
 )
